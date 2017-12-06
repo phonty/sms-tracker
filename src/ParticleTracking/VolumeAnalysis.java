fd0d377c72a3d6edcc64e6c3dcddeb760668cd42
davejpbarry <davejpbarry@gmail.com>
2017-12-06 15:55:21 +0000

    Modified UserInterface to use detectionPanel
    Corrected error in detectionPanel.detectionModeComboBoxActionPerformed
    
    Minor changes to ParticleTracker to reflect changes elsewhere

--- src/ParticleTracking/VolumeAnalysis.java
+++ src/ParticleTracking/VolumeAnalysis.java
@@ -248,7 +248,7 @@ public class VolumeAnalysis extends Particle_Tracker {
             }
         }
         if (update) {
-            TrajectoryBuilder.updateTrajectories(particles, timeRes, UserVariables.getTrajMaxStep(), spatialRes, true, 1.0, trajectories, false);
+            TrajectoryBuilder.updateTrajectories(particles, timeRes, UserVariables.getTrajMaxStep(), spatialRes, 1.0, trajectories, false);
         }
         return particles;
     }
